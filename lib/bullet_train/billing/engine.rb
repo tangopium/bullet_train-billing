d6bed67efa62095a45a8d6551df8df19e20b696a
Jeremy Green <jeremy@octolabs.com>
2023-08-18 12:05:23 -0500

    Make our factories available to consuming apps

--- lib/bullet_train/billing/engine.rb
+++ lib/bullet_train/billing/engine.rb
@@ -1,6 +1,16 @@
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+
 module BulletTrain
   module Billing
     class Engine < ::Rails::Engine
+      if defined? FactoryBotRails
+        # TODO: We should probably move the factories out of the test directory and into lib so that we can
+        # ship them for use by consuming apps. For now this works when linking against a local copy.
+        config.factory_bot.definition_file_paths += [File.expand_path('../../../../test/factories', __FILE__)]
+      end
     end
   end
 end
