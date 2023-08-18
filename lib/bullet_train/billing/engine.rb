90102c12aa3d07d13876604fd24d2371e5a3e52c
Jeremy Green <jeremy@octolabs.com>
2023-08-18 13:51:43 -0500

    Don't auto merge factor paths

--- lib/bullet_train/billing/engine.rb
+++ lib/bullet_train/billing/engine.rb
@@ -1,14 +1,14 @@
-begin
-  require "factory_bot_rails"
-rescue LoadError
-end
+#begin
+  #require "factory_bot_rails"
+#rescue LoadError
+#end
 
 module BulletTrain
   module Billing
     class Engine < ::Rails::Engine
-      if defined? FactoryBotRails
-        config.factory_bot.definition_file_paths += [File.expand_path("../../../factories", __FILE__)]
-      end
+      #if defined? FactoryBotRails
+        #config.factory_bot.definition_file_paths += [File.expand_path("../../../factories", __FILE__)]
+      #end
     end
   end
 end
---
+++ lib/bullet_train/billing/test_support.rb
@@ -0,0 +1,7 @@
+module BulletTrain
+  module Billing
+    class TestSupport
+      FACTORY_PATH = File.expand_path("../../../factories", __dir__)
+    end
+  end
+end
