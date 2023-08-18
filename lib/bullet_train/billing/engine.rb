3e7a0d16f5e76960390df279868878ac999badb2
Jeremy Green <jeremy@octolabs.com>
2023-08-18 13:13:37 -0500

    Move factories into lib so that we ship them

--- lib/bullet_train/billing/engine.rb
+++ lib/bullet_train/billing/engine.rb
@@ -7,9 +7,7 @@ module BulletTrain
   module Billing
     class Engine < ::Rails::Engine
       if defined? FactoryBotRails
-        # TODO: We should probably move the factories out of the test directory and into lib so that we can
-        # ship them for use by consuming apps. For now this works when linking against a local copy.
-        config.factory_bot.definition_file_paths += [File.expand_path("../../../../test/factories", __FILE__)]
+        config.factory_bot.definition_file_paths += [File.expand_path("../../../factories", __FILE__)]
       end
     end
   end
