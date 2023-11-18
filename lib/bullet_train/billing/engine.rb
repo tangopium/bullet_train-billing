573ad08466c20db403b2d31015924d0d3079325f
Julian Rubisch <julian@julianrubisch.at>
2023-11-18 16:36:06 +0100

    feat: Notify provider gems of quantity changes

--- lib/bullet_train/billing/engine.rb
+++ lib/bullet_train/billing/engine.rb
@@ -1,6 +1,25 @@
 module BulletTrain
   module Billing
     class Engine < ::Rails::Engine
+      initializer "bullet_train-billing.quantity" do
+        ActiveSupport::Notifications.subscribe("memberships.quantity-changed") do |_name, _start, _finish, _id, payload|
+          billing_subscription = payload[:team].billing_subscriptions.first
+
+          next unless billing_subscription.present?
+
+          included_price = billing_subscription.included_prices.first
+
+          # if not billing per seat, return
+          next unless included_price.price[:quantity] == "memberships"
+
+          new_quantity = included_price.price.calculate_quantity(payload[:team])
+
+          # return if quantity hasn't changed
+          next if included_price[:quantity] == new_quantity
+          
+          ActiveSupport::Notifications.instrument("memberships.provider-quantity-changed", {provider_subscription: billing_subscription.provider_subscription, included_price:, quantity: new_quantity})
+        end
+      end
     end
   end
 end
